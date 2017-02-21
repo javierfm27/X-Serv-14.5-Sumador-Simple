735d3e5255503a26d94bc5f2e91cb0c78d298a94
javierfm27 <javitofm27@github.com>
2017-02-21 17:35:35 +0100

    Cambios tras el check.py

--- servidor-sumador.py
+++ servidor-sumador.py
@@ -10,33 +10,33 @@ state = 0
 try:
     while True:
         print("Waiting conections")
-        (recvSocket,address) = mySocket.accept()
+        (recvSocket, address) = mySocket.accept()
         print("Request received")
-        peticion = recvSocket.recv(2048).decode('utf-8','strict')
+        peticion = recvSocket.recv(2048).decode('utf-8', 'strict')
         print(peticion)
         peticion = peticion.split()
         if state == 0:
             state = 1
             operando1 = peticion[1][1:]
             recvSocket.send(bytes("HTTP/1.1 200 OK \r\n\r\n" +
-                                "<!DOCTYPE html> <html><body>" +
-                                "<h5>El primer operando es  " +
-                                str(operando1) +
-                                ", introduzca el segundo operando" +
-                                "</h5>" +
-                                "</body></html>\r\n","utf-8"))
+                                  "<!DOCTYPE html> <html><body>" +
+                                  "<h5>El primer operando es  " +
+                                  str(operando1) +
+                                  ", introduzca el segundo operando" +
+                                  "</h5>" +
+                                  "</body></html>\r\n", "utf-8"))
         else:
             operando2 = peticion[1][1:]
             resultado = int(operando1) + int(operando2)
             recvSocket.send(bytes("HTTP/1.1 200 OK \r\n\r\n" +
-                                "<!DOCTYPE html><html><body>" +
-                                "<h4> El resultado de sumar " +
-                                operando1 +
-                                " y " +
-                                operando2 +
-                                " es " +
-                                str(resultado) +
-                                "</h4></body></html>\r\n","utf-8"))
+                                  "<!DOCTYPE html><html><body>" +
+                                  "<h4> El resultado de sumar " +
+                                  operando1 +
+                                  " y " +
+                                  operando2 +
+                                  " es " +
+                                  str(resultado) +
+                                  "</h4></body></html>\r\n", "utf-8"))
             state = 0
         recvSocket.close()
 except KeyboardInterrupt:
